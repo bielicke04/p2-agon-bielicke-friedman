1aa458b693aeb6fa08f2e25990b3b69c0fc16bf1
EJ Agon Kresky <ej.agonkresky@ufl.edu>
2021-07-15 01:35:18 -0400

    Style sticky header form, move nav JS to separate file

---
+++ nav.js
@@ -0,0 +1,78 @@
+// Sticky Header https://www.w3schools.com/howto/howto_js_sticky_header.asp
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("sticky-header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+// login/local storage JS from Lee's lecture
+
+var form = document.querySelector('form')
+var firstNameInput = document.getElementById('firstName')
+var lastNameInput = document.getElementById('lastName')
+var highSchoolInput = document.getElementById('highSchool')
+var nameEl = document.getElementById('name')
+var clear = document.querySelector('#clear')
+
+var firstName = localStorage.getItem('first')
+var lastName = localStorage.getItem('last')
+var highSchool = localStorage.getItem('school')
+
+if (firstName && lastName && highSchool) {
+  nameEl.textContent = firstName + " " + lastName + " from " + highSchool
+  firstNameInput.value = firstName
+  lastNameInput.value = lastName
+  highSchoolInput.value = highSchool
+} else {
+  nameEl.textContent = 'Already registered with Broadway Bridges? Please login below!'
+}
+
+form.onsubmit = function (e) {
+  e.preventDefault()
+  firstName = firstNameInput.value
+  lastName = lastNameInput.value
+  highSchool = highSchoolInput.value
+  localStorage.setItem('first', firstName)
+  localStorage.setItem('last', lastName)
+  localStorage.setItem('school', highSchool)
+  nameEl.textContent = "Welcome back, " + firstName + " " + lastName + " from " + highSchool
+}
+
+clear.onclick = function () {
+  firstNameInput.value = ""
+  lastNameInput.value = ""
+  highSchoolInput.value = ""
+  localStorage.setItem("first", "")
+  localStorage.setItem("last", "")
+  localStorage.setItem("school", "")
+  nameEl.textContent = 'Already registered with Broadway Bridges? Please login below!'
+}
+
+
+
+
+// hamburger menu vanilla JS https://codepen.io/sitanotern1337/pen/xyQppZ
+
+var hamburger = document.querySelector('.hamburger');
+var menu = document.querySelector('.navbar');
+var bod = document.querySelector('.container');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('isactive');
+  menu.classList.toggle('active');
+
+});
